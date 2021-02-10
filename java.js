9b1c0961a753ea8e66de73d0005dbd933aeb50d4
startbyzero <62246453+startbyzero@users.noreply.github.com>
2021-02-10 12:05:25 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+
+/*Over pictures*/
+function newPic(){
+  document.getElementById("one").src = "/java6.jpg";
+}
+function oldPic(){
+ document.getElementById("one").src = "/java1.jpg";
+}
+function newPic1(){
+  document.getElementById("two").src = "/java7.jpg";
+}
+function oldPic1(){
+ document.getElementById("two").src = "/java2.jpg";
+}
+function newPic2(){
+  document.getElementById("three").src = "/java8.jpg";
+}
+function oldPic2(){
+ document.getElementById("three").src = "/java3.jpg";
+}
+function newPic3(){
+  document.getElementById("four").src = "/java9.jpg";
+}
+function oldPic3(){
+ document.getElementById("four").src = "/java4.jpg";
+}
+//End Over Pictures
+
+
